c37a3645a2ca063d56be0ba0ea1eb5ff1581fc79
Dalton Kajander <daltonkajander@yahoo.com>
2025-11-17 21:24:31 -0500

    hello

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # MagicSim
 
+## TODO
+### Basic gameplay loop
+* Cards in hand, library, and graveyard
+* Untap, draw, main phase 1, combat (declare attackers, declare blockers, damage), main phase 2, end, cleanup
+* Players have life
+* Vanilla creatures for now
+* Only lands and creatures for now
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello, world!")
+from dataclasses import dataclass, field
+
+@dataclass
+class Card:
+    name: str
+    card_types: list[str] = field(default_factory=list[str])
+    card_subtypes: list[str] = field(default_factory=list[str])
+    
+    
+@dataclass
+class CreatureCard(Card):
+    ...
+    
+    
+    
+if __name__ == "__main__":
+    bear = CreatureCard("Bear")
+    
+    print(bear)
